2f67c3f3925525d0488f45a5e7906f08067ee2ab
msmalmir <97928189+msmalmir@users.noreply.github.com>
2025-07-03 21:14:14 +0000

    adding file CLI

---
+++ wikiphrases.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python
+import fire
+from corenlp import get_phrases
+
+if __name__ == "__main__":
+    fire.Fire(get_phrases)  # Use fire to create a command-line interface for get_phrases function
